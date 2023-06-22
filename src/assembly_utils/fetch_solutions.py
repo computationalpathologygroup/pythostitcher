e7e16162118e65d893c57ec233ddfe8c7a44d213
dnschouten <daan.schouten@radboudumc.nl>
2023-06-22 12:53:27 +0200

    fixed top-n solution sorting bug
    Apparently the top-n solutions were not sorted based on their MSE but on JigsawNet score, fixed this.

--- src/assembly_utils/fetch_solutions.py
+++ src/assembly_utils/fetch_solutions.py
@@ -14,32 +14,38 @@ def fetch_solutions(parameters):
         contents = [i.rstrip("\n") for i in contents]
         contents = [i.split(",") for i in contents]
 
-        # Sort mean squared error scores. Lower = better.
-        scores = [int(i[0].split(":")[-1]) for i in contents]
-        sort_idx = np.argsort(scores)
+    # Sort mean squared error scores. Lower = better.
+    scores = [int(i[0].split(":")[-1]) for i in contents]
+    sort_idx = np.argsort(scores)
 
-        # Gather all solutions
-        final_solutions = []
-        original_filenames = sorted(
-            [
-                i.name
-                for i in parameters["data_dir"].joinpath("raw_images").iterdir()
-                if not i.is_dir()
-            ]
-        )
+    # Gather all solutions
+    final_solutions = []
+    original_filenames = sorted(
+        [
+            i.name
+            for i in parameters["data_dir"].joinpath("raw_images").iterdir()
+            if not i.is_dir()
+        ]
+    )
 
-        # Only save 3 best results
-        for c, idx in enumerate(sort_idx[:3]):
-            temp_sol = dict()
+    # Only save top k best results scored by mse for actual full-res stitching. You
+    # can change this number based on your requirements.
+    top_k = 3
+    for s in sort_idx[:top_k]:
+        temp_sol = dict()
 
-            # Relate each fragment pseudonym back to its original name
-            for element in contents[c][1:]:
-                idx = int(element.split(".png")[0].lstrip("fragment")) - 1
-                filename = original_filenames[idx]
-                location = element.split(":")[-1]
-                temp_sol[filename] = location
+        # Relate each fragment pseudonym back to its original name
+        for element in contents[s][1:]:
 
-            final_solutions.append(temp_sol)
+            # Sanity check to ensure good scores
+            assert int(contents[s][0].lstrip("mse:")) == scores[s]
+
+            idx = int(element.split(".png")[0].lstrip("fragment")) - 1
+            filename = original_filenames[idx]
+            location = element.split(":")[-1]
+            temp_sol[filename] = location
+
+        final_solutions.append(temp_sol)
 
     parameters["log"].log(
         parameters["my_level"], f"Examining {len(final_solutions)} best solutions...\n"
